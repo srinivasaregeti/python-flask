a4468d2a7c8ad03bba304b5cd1c88ea90a77d7fc
Srinivasa Regeti <regeti@regeti.com>
2020-08-14 13:47:30 -0400

    updated test case with google.com

--- testcase-01.js
+++ testcase-01.js
@@ -1,53 +1,14 @@
 
 "use strict"; 
-var webdriver = require("selenium-webdriver"),
-SeleniumServer = require("selenium-webdriver/remote").SeleniumServer;
- 
-var cbtHub = "http://hub.crossbrowsertesting.com:80/wd/hub";
+var webdriver = require("selenium-webdriver");
+var browser = new webdriver.Builder().withCapabilities(webdriver.Capabilities.firefox()).build();
 
-var username ='YOUR_USERNAME'; //replace with your email address 
-var authkey = 'YOUR_AUTHKEY'; //replace with your authkey  
+browser.get('http://www.google.com');
 
-var caps = {
-    name : 'Basic Test Example',
-    build :  '1.0',
-    version : '70', 
-    platform : 'Windows 10', 
-    screen_resolution : '1366x768',
-    record_video : 'true',
-    record_network : 'false',
-    browserName : 'Chrome',
-    username : username,
-    password : authkey
-};
+var promise = browser.getTitle();
 
+promise.then(function(title) {
+    console.log(title)
+});
 
-async function basicExample(){
-    try{
-        var driver = new webdriver.Builder()
-            .usingServer(cbtHub)
-            .withCapabilities(caps)
-            .build(); 
-
-
-        await driver.get('http://crossbrowsertesting.github.io/selenium_example_page.html');
-
-        await driver.getTitle().then(function(title) {
-                    console.log("The title is: " + title)
-            });
-
-        driver.quit();
-    }
-
-    catch(err){
-        handleFailure(err, driver)
-    }
-
-}
-
-basicExample();
-
-function handleFailure(err, driver) {
-     console.error('Something went wrong!\n', err.stack, '\n');
-     driver.quit();
-}
+browser.quit()
