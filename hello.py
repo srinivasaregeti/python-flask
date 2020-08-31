c920e6400214820241b3ba6a1c9c6538b5433fdb
Srinivasa R Regeti <regeti@regeti.com>
2020-08-31 12:09:17 -0400

    Updating all files

---
+++ hello.py
@@ -0,0 +1,16 @@
+import os
+from flask import Flask, json
+
+ids = [{"id": 1, "name": "Company One"}, {"id": 3, "name": "Company Three"}]
+
+api = Flask(__name__)
+
+
+@api.route('/ids', methods=['GET'])
+def get_ids():
+    return json.dumps(ids)
+
+
+port = os.getenv('PORT', '5000')
+if __name__ == "__main__":
+    api.run(host='0.0.0.0', port=int(port))
---
+++ testcase-01.js
@@ -0,0 +1,14 @@
+
+"use strict"; 
+var webdriver = require("selenium-webdriver");
+var browser = new webdriver.Builder().withCapabilities(webdriver.Capabilities.firefox()).build();
+
+browser.get('http://www.google.com');
+
+var promise = browser.getTitle();
+
+promise.then(function(title) {
+    console.log(title)
+});
+
+browser.quit()
